77ea5b21e8af3a77b5cb7d25bffadc65adc2b74b
Clau <inda.claudia@gmail.com>
2021-09-16 20:18:36 -0500

    mi primer programa

---
+++ holamundo.py
@@ -0,0 +1,12 @@
+print("holamundo")
+message1=("hola")
+message2=("continua")
+message3=("fin")
+print(message1.upper())
+print(message2.lower())
+print(message3.upper())
+#Los comentarios se agregan al iniciar un simbolo de gato
+NOMBRE="CLAU"
+APELLIDO="INDA"
+NOMBRE_COMPLETO=NOMBRE+" "+APELLIDO
+print(NOMBRE_COMPLETO)
