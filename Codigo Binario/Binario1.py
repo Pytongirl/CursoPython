580ac4a4450f32239478e5e5188a34abefa6f837
Clau <inda.claudia@gmail.com>
2021-09-29 17:23:46 -0500

    Ejercicio1 Binario falta eliminat tag b

---
+++ Codigo Binario/Binario1.py
@@ -0,0 +1,4 @@
+numero = input ("Ingresa el numero decimal deseado")
+numero= int(numero)
+binario = bin(numero)
+print(f"numero decimal={numero} en codigo binario: {binario}")
---
+++ Ejercicios_String/codigo Ascii.py
@@ -0,0 +1,9 @@
+caracter = input ("Ingresa el valor del caracter deseado")
+ascii = ord(caracter)
+print(f"numero ordinal numero={caracter} en codigo ascii: {ascii}")
+#lado = int (lado)
+
+#area es lado al cuadrado
+#area = lado ** 2
+#print(f"El area de tu cuadrado con l={lado} es: {area}")
+#print("El area del cuadrado con l=" + str(lado) + " es: " + str(area))
